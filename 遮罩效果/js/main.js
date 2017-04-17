4a6e8796e1632cefc3a87c317676d9c80c234263
LiHongyao <lihy_online@163.com>
2017-04-17 17:39:55 +0800

    内容更新

---
+++ 遮罩效果/js/main.js
@@ -0,0 +1,44 @@
+/**
+ * Created by LiHongyao on 2017/4/17.
+ */
+
+let oClickBtn   = document.getElementById('clickBtn');
+let oCloseBtn   = document.getElementById('closeBtn');
+let oContentBox = document.getElementById('content-box');
+
+
+let sWidth = document.body.clientWidth;
+let sHeight = document.body.clientHeight;
+
+let oMask = document.createElement('div');
+
+oMask.style.cssText = `width: ${sWidth}px; height: ${sHeight}px;`;
+console.log(oMask)
+oMask.className = 'mask';
+
+// events
+oClickBtn.onclick = function() {
+    document.body.insertBefore(oMask, document.scripts[0]);
+    oContentBox.style.display = 'table';
+}
+oCloseBtn.onclick = function () {
+    document.body.removeChild(oMask);
+    oContentBox.style.display = 'none';
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
